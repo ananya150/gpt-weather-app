454a1a7a8a5c601fef6a63c9001a656d02fe869c
ananya150 <akblockchain15@gmail.com>
2023-06-01 10:22:40 +0530

    feat: added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
+import { Card , Divider , Subtitle , Text } from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="">
-      A weather app with gpt
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center">
+      <Card className="max-w-2xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">Weather GPT</Text>
+        <Subtitle className="text-xl text-center">Powered by OpenAI , NextJS , Tailwindcss , Tremor & more</Subtitle>
+      
+        <Divider className="my-10" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+
+        </Card>
+
+      </Card>
+    </div>
   )
 }
