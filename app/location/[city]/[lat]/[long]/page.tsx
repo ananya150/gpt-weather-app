7e66a570a99d26d39fcadc7bbc2d66a3da7ac704
ananya150 <akblockchain15@gmail.com>
2023-06-01 14:30:25 +0530

    feat: added stat cards

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { getClient } from '@/apollo-client'
 import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries'
 import CalloutCard from '@/components/CalloutCard'
+import StatCard from '@/components/StatCard'
 
 type Props = {
     params:{
@@ -45,6 +46,27 @@ const WeatherPage = async ({params: {city , lat , long}}: Props) => {
 
           <div>
             <CalloutCard message='This is where gpt-4 summary will go!' />
+          </div>
+
+          <div className='grid grid-cols-1 xl:grid-cols-2 gap-5 m-2'>
+            <StatCard title='Maximum Temperature' metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`} color="yellow" />
+            <StatCard title='Minimum Temperature' metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`} color="green" />
+          
+            <div>
+              <StatCard title='UV Index' metric={`${results.daily.uv_index_max[0].toFixed(1)}`} color="rose" />
+              {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                <CalloutCard
+                  message='The UV is high today, be sure to wear SPF!'
+                  warning
+                  />
+                )
+              }
+            </div>
+            <div className='flex space-x-3'>
+              <StatCard title='Wind Speed' metric={`${results.current_weather.windspeed.toFixed(1)}m/s`} color="cyan" />
+              <StatCard title='Wind Direction' metric={`${results.current_weather.winddirection.toFixed(1)}°`} color="violet" />
+            </div>
+
           </div>
 
         </div>  
